981a59ed16c4c2356d632589d7d77de6c142b833
riapebriandini <riapebriandini98109@gmail.com>
2023-09-28 12:29:31 +0700

    Tambah output

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,10 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        
+        System.out.print("Nama" + ":" + " ");
+        System.out.println("Ria Pebrian Dini");
+        System.out.print("NIM" + ":" + " ");
+        System.out.println("607062330010");
+        System.out.print("Kelas" + ":" + " ");
+        System.out.println("D3IF-47-01");
     }
 }
